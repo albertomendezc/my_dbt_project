049a17d40764dc7afd1e637b1dc6bbd35c066797
Alberto MC <alberto.mendezc@proton.me>
2025-03-03 23:18:14 +0000

    fixed formula

--- models/staging/cas_final/stg_order_items.sql
+++ models/staging/cas_final/stg_order_items.sql
@@ -5,6 +5,6 @@ SELECT
     quantity,
     list_price,
     discount,
-    ROUND(list_price * discount,2) AS final_price,
-    ROUND(SUM(list_price * discount) OVER (PARTITION BY order_id),2) AS total_order_price
+    ROUND(list_price * (1-discount),2) AS final_price,
+    ROUND(SUM(list_price * (1-discount)) OVER (PARTITION BY order_id),2) AS total_order_price
 FROM {{ source('cas_final', 'src_order_items') }}
