2d5fdd7142fa1228ecb52a0aadabeac7186ad0fb
Alberto MC <alberto.mendezc@proton.me>
2025-03-03 23:43:04 +0000

    added new concat field

--- models/intermediate/cas_final/int_product_category.sql
+++ models/intermediate/cas_final/int_product_category.sql
@@ -6,7 +6,8 @@ SELECT
     prd.model_year,
     prd.list_price,
     cat.category_name,
-    brands.brand_name
+    brands.brand_name,
+    CONCAT(brands.brand_name,"-",cat.category_name) AS brand_and_category
 FROM {{ ref('stg_products') }} AS prd
 LEFT JOIN {{ ref('stg_categories') }} AS cat ON prd.category_id = cat.category_id
 LEFT JOIN {{ ref('stg_brands') }} AS brands ON prd.brand_id = brands.brand_id
